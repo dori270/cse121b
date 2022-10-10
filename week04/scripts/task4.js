0a296ec5ccd54ec3c4d2e62bf57c55bf14f0797d
dori270 <dori270@yahoo.com>
2022-10-10 14:52:57 -0400

    Still, nothing works...

--- week04/scripts/task4.js
+++ week04/scripts/task4.js
@@ -5,14 +5,14 @@
 // Step 1: Declare a new variable to hold information about yourself
 
 const bioInfo = {
-    "name": "Savannah Rucker",
-    "photo": "images\myPhoto.jpeg",
-    "favoriteFoods": ["falafel", "hummus", "lasagne", "cereal"],
-    "hobbies": ["coding", "teaching", "making school lunches"],
-    "placesLived": {
-        "place": {"Albuquerque": "New Mexico", "Springfield": "Illinois"},
-        "length": {"4 years": "Land of Enchantment", "8 years": "Land of Lincoln"}
-    },
+    name: "Savannah Rucker",
+    photo: "images\myPhoto.jpeg",
+    favoriteFoods: ["falafel", "hummus", "lasagne", "cereal"],
+    hobbies: ["coding", "teaching", "making school lunches"],
+    placesLived: [
+        place: {"Albuquerque": "New Mexico", "Springfield": "Illinois"},
+        length: {"4 years": "Land of Enchantment", "8 years": "Land of Lincoln"}
+    ],
 };
 
 
@@ -37,34 +37,50 @@ const bioInfo = {
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
+//function setInfo(bio){
+    document.querySelector("#name").innerHTML = bioInfo.name;
+//}
 
-bioInfo.name = document.querySelector("#name").setAttribute
 
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
 
-document.querySelector("#photo").value = bioInfo.photo
+document.querySelector("#photo").innerHTML = bioInfo.photo;
 
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
 
-
+document.getElementById("img").value= bioInfo.name; 
+//This isn't right, because "img" isn't an HTML id...
 
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
 
 let favFoods = document.getElementById("favorite-Foods");
 
+
+
 for (let i = 0; i < favoriteFoods.length; i++){
     let listItem = document.createElement("li");
-    listItem.textContent = favoriteFoods[i]; 
-    favorite-Foods.append(listItem);
-}
-
-
+    listItem.textContent = bioInfo.favoriteFoods[i]; 
+    
 
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
 
+favorite-Foods.append(listItem);
+}
+
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
 
+let hobbies = document.querySelector("#hobbies");
+
+for (let i = 0; i < hobbies.length; i++){
+    let listItem = document.createElement("li");
+    listItem.textContent = bioInfo.hobbies[i]; 
+    hobbies.append(listItem);
+}
+    
+
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
+
+
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
