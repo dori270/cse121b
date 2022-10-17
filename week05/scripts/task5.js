ef9ce3df2ce5a4aaa39a626c712b5d3d33879f9e
dori270 <dori270@yahoo.com>
2022-10-17 12:52:49 -0400

    tried to fix a few things...

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -81,7 +81,10 @@ const templeList = [ ];
 
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
 
-const output = (temples) => {
+const output = (templeList) => {
+    templeList.forEach(element => {
+        
+    });
     
 // - Creates an HTML <article> element
 
@@ -94,6 +97,7 @@ const output = (temples) => {
 // - Creates an HTML <h4> element and add the temple's location property to it
 
     const newH4 = document.createElement("h4").location; 
+//^^^^^is this correct?
 
 // - Creates an HTML <h4> element and add the temple's dedicated property to it
 
@@ -102,17 +106,18 @@ const output = (temples) => {
 
 // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
 
-    const imgHTML = document.createElement("img").imageUrl = 
-    imgHTML.setAttribute("imageURL") = document.querySelector("#").value(src);
-     //= document.value
+//^^^^^^^This is throwing me for a loop, no pun intended...
+    //const imgHTML = document.createElement("img").imageUrl 
+    //imgHTML.setAttribute("imageURL") = document.querySelector("#").value(src);
 
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 
-    newArticle.append(newH3, newH4, newH42, imgHTML);
+//^^^^^^^^imgHTML is not defined........
+    //c newArticle.append(newH3, newH4, newH42, imgHTML);
 
 // - Appends the <article> element to the HTML element with an ID of temples
 
-    document.append(newArticle, id=temples);
+    document.querySelector("#temples").append(newArticle, id=temples);
 }
 
 // Step 3: Create another function called getTemples. Make it an async function.
@@ -121,13 +126,29 @@ async function getTemples() {
 
 // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
 
-    }
+    const response = await fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json");
+
 // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
+
+    const templeList = await response.json();
+}
 // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+
+output(templeList)
 
 // Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
 
+function reset(){
+    document.getElementById(temples).innerHTML = " "
+}
+
 // Step 8: Declare a function named sortBy that does the following:
+
+function sortBy(reset){
+    //object.querySelector("#sortBy").value = 
+
+}
+
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
 // - Calls the output function passing in the sorted list of temples
