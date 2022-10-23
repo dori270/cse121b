ab6035ec31d6042f4454a88848309a818b32b4d1
dori270 <dori270@yahoo.com>
2022-10-23 00:00:37 -0400

    input fields

---
+++ week06/main.js
@@ -0,0 +1,30 @@
+
+
+function costPerMile(vehiclePrice, remainingMileage){
+    const value = vehiclePrice/ remainingMileage;
+    return value;
+}
+
+
+
+
+// initialize input variables
+let input1 = " "
+let input2 = " "
+let input3 = " "
+let input4 = " "
+let input5 = " "
+
+// set the selectors
+const vehicleName1Element = document.querySelector("#vehicleName1");
+const carName2Element = document.querySelector("#carName2");
+const carName3Element = document.querySelector("#carName3");
+const carName4Element = document.querySelector("#carName4");
+const carName5Element = document.querySelector("#carName5");
+
+// set the innerHTML properties of the selectors
+carNameElement1.innerHTML = input1;
+carName2Element.innerHTML = input2;
+carName3Element.innerHTML = input3;
+carName4Element.innerHTML = input4;
+carName5Element.innerHTML = input5; 
