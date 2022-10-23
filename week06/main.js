1dd6ae9de64f971a70c70113d286a1a7f6a93f35
dori270 <dori270@yahoo.com>
2022-10-23 00:43:04 -0400

    working on costPerMile

--- week06/main.js
+++ week06/main.js
@@ -1,30 +1,27 @@
+const listPriceElement= document.querySelector("#listPrice");
+const odometerElement= document.querySelector("#odometer");
+
+listPriceElement.innerHTML = listPrice.value;
 
 
-function costPerMile(vehiclePrice, remainingMileage){
-    const value = vehiclePrice/ remainingMileage;
-    return value;
+function costPerMile (listPrice, odometer) {
+    const vehicleValue = listPrice/ (200000- odometer);
+    return vehicleValue;
 }
 
+const button = document.querySelector("#calculate");
+button.addEventListener("click", costPerMile(listPrice, odometer));
 
-
+const vehicleValue.innerHTML = vehicleValue.value; 
 
 // initialize input variables
 let input1 = " "
-let input2 = " "
-let input3 = " "
-let input4 = " "
-let input5 = " "
+
 
 // set the selectors
 const vehicleName1Element = document.querySelector("#vehicleName1");
-const carName2Element = document.querySelector("#carName2");
-const carName3Element = document.querySelector("#carName3");
-const carName4Element = document.querySelector("#carName4");
-const carName5Element = document.querySelector("#carName5");
+
 
 // set the innerHTML properties of the selectors
 carNameElement1.innerHTML = input1;
-carName2Element.innerHTML = input2;
-carName3Element.innerHTML = input3;
-carName4Element.innerHTML = input4;
-carName5Element.innerHTML = input5; 
+
